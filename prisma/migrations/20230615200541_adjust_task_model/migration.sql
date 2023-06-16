b2132dec69ddaf0bb962c76c486aafe8e2595cf0
Hoffmann-Ferreira <andreihf@outlook.com>
2023-06-16 18:04:28 -0300

    feat: task route

---
+++ prisma/migrations/20230615200541_adjust_task_model/migration.sql
@@ -0,0 +1,20 @@
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Task" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "concluded" BOOLEAN NOT NULL DEFAULT false,
+    "name" TEXT NOT NULL,
+    "date" DATETIME NOT NULL,
+    "task" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "userId" TEXT NOT NULL,
+    "taskId" TEXT,
+    CONSTRAINT "Task_taskId_fkey" FOREIGN KEY ("taskId") REFERENCES "Task" ("id") ON DELETE SET NULL ON UPDATE CASCADE,
+    CONSTRAINT "Task_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+INSERT INTO "new_Task" ("concluded", "createdAt", "date", "id", "name", "task", "taskId", "userId") SELECT "concluded", "createdAt", "date", "id", "name", "task", "taskId", "userId" FROM "Task";
+DROP TABLE "Task";
+ALTER TABLE "new_Task" RENAME TO "Task";
+CREATE UNIQUE INDEX "Task_id_key" ON "Task"("id");
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
--- src/routes/tasks.ts
+++ src/routes/tasks.ts
@@ -1,6 +1,29 @@
 import { FastifyInstance } from 'fastify';
+import { prisma } from '../lib/prisma';
+import { z } from 'zod';
 
 export async function tasksRoutes(server: FastifyInstance) {
+  server.post('/creating-task', async (request) => {
+    const bodySchema = z.object({
+      name: z.string(),
+      date: z.string(),
+      task: z.string(),
+    });
+
+    const { name, date, task } = bodySchema.parse(request.body);
+
+    const createdTask = await prisma.task.create({
+      data: {
+        name,
+        date,
+        task,
+        userId: '3ee7b3d5-e897-44b6-9231-31924b76a0b2',
+      },
+    });
+
+    return createdTask;
+  });
+
   server.get('/tasks', async (request, reply) => {
     return 'Hello world';
   });
