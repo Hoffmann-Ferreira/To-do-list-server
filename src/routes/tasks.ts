1244bc00d24a4123cf06697df972e4d4944682c8
Hoffmann-Ferreira <andreihf@outlook.com>
2023-06-20 15:12:10 -0300

    feat: delete task route

--- src/routes/tasks.ts
+++ src/routes/tasks.ts
@@ -1,7 +1,6 @@
 import { FastifyInstance } from 'fastify';
 import { prisma } from '../lib/prisma';
-import { z } from 'zod';
-import { request } from 'http';
+import { string, z } from 'zod';
 
 export async function tasksRoutes(server: FastifyInstance) {
   server.post('/creating-task', async (request) => {
@@ -23,6 +22,34 @@ export async function tasksRoutes(server: FastifyInstance) {
     });
 
     return createdTask;
+  });
+
+  server.post('/creat-sub-task/:id', async (request) => {
+    const paramsSchema = z.object({
+      id: z.string().uuid(),
+    });
+
+    const { id } = paramsSchema.parse(request.params);
+
+    const bodySchema = z.object({
+      name: z.string(),
+      date: z.string(),
+      task: z.string(),
+    });
+
+    const { name, date, task } = bodySchema.parse(request.body);
+
+    const creatSubTask = await prisma.task.create({
+      data: {
+        name,
+        date,
+        task,
+        userId: '3ee7b3d5-e897-44b6-9231-31924b76a0b2',
+        taskId: id,
+      },
+    });
+
+    return creatSubTask;
   });
 
   server.patch('/edit-task/:id', async (request) => {
@@ -75,49 +102,35 @@ export async function tasksRoutes(server: FastifyInstance) {
     return tasks;
   });
 
-  server.post('/creat-sub-task/:id', async (request) => {
+  server.get('/task/:id', async (request) => {
     const paramsSchema = z.object({
       id: z.string().uuid(),
     });
 
     const { id } = paramsSchema.parse(request.params);
 
-    const bodySchema = z.object({
-      name: z.string(),
-      date: z.string(),
-      task: z.string(),
-    });
-
-    const { name, date, task } = bodySchema.parse(request.body);
-
-    const creatSubTask = await prisma.task.create({
-      data: {
-        name,
-        date,
-        task,
-        userId: '3ee7b3d5-e897-44b6-9231-31924b76a0b2',
-        taskId: id,
-      },
-    });
-
-    return creatSubTask;
-  });
-
-  server.get("/taks/:id", async(request) => {
-    const paramsSchema = z.object({
-      id: z.string().uuid()
-    })
-
-    const {id} = paramsSchema.parse(request.params)
-
     const tasks = await prisma.task.findFirstOrThrow({
       where: {
-        id: id
+        id: id,
       },
-    })
+    });
 
-    return tasks
-  })
+    return tasks;
+  });
 
-  
+  server.delete('/delete-task/:id', async (request) => {
+    const paramsSchema = z.object({
+      id: string().uuid(),
+    });
+
+    const { id } = paramsSchema.parse(request.params);
+
+    const deleteTask = await prisma.task.delete({
+      where: {
+        id: id,
+      },
+    });
+
+    return deleteTask;
+  });
 }
